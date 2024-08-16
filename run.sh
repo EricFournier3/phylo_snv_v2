98e0ec0c4214f63a18303d766253fce732e606ef
Éric Fournier <foueri01@inspq.qc.ca@slurm10p.inspq.qc.ca>
2024-08-16 15:40:40 -0400

    ef

--- run.sh
+++ run.sh
@@ -14,7 +14,12 @@ species=$(sed -n '/^species/p' ${config_file} | sed "s/\"//g" |  sed -E "s/\s+//
 outdir_name=$(sed -n '/^outdir_name/p' ${config_file} | sed "s/\"//g" |  sed -E "s/\s+//g" | sed "s/=//g" | sed "s/outdir_name//g")
 
 snv_phyl_res_dir=$(sed -n '/^snv_phyl_res_dir/p' ${config_file} | sed "s/\"//g" |  sed -E "s/\s+//g" | sed "s/=//g" | sed "s/snv_phyl_res_dir//g")
+
 snv_phyl_res_dir=${out_basedir}/${species}/${outdir_name}/${snv_phyl_res_dir}
+
+partage_basedir=$(sed -n '/^partage_basedir/p' ${config_file} | sed "s/\"//g" |  sed -E "s/\s+//g" | sed "s/=//g" | sed "s/partage_basedir//g")
+
+current_partage_basedir="${partage_basedir}"/"${outdir_name}"
 
 snv_phyl_samplesheet=$(sed -n '/^snv_phyl_samplesheet/p' ${config_file} | sed "s/\"//g" |  sed -E "s/\s+//g" | sed "s/=//g" | sed "s/^snv_phyl_samplesheet//g")
 snv_phyl_samplesheet=${out_basedir}/${species}/${outdir_name}/${snv_phyl_samplesheet}
@@ -27,22 +32,32 @@ module load nextflow/24.04.4
 export NXF_HOME=/data/devel/phylo_snv_v2/.nextflow
 export NXF_OPTS='-Xms1g -Xmx4g'
 
-nextflow run main.nf -process.echo -profile slurm --myworflow prepare
-nextflow run main.nf -process.echo -profile slurm --myworflow ksnp3
+current_wf="prepare"
+nextflow run main.nf -process.echo -profile slurm --myworflow ${current_wf} -with-report "${current_partage_basedir}"/nextflow_report/${current_wf}_report.html -with-timeline "${current_partage_basedir}"/nextflow_report/${current_wf}_timeline.html -with-dag "${current_partage_basedir}"/nextflow_report/${current_wf}_dag.html -with-trace  "${current_partage_basedir}"/nextflow_report/${current_wf}_trace.txt 
+
+current_wf="ksnp3"
+nextflow run main.nf -process.echo -profile slurm --myworflow ${current_wf} -with-report "${current_partage_basedir}"/nextflow_report/${current_wf}_report.html -with-timeline "${current_partage_basedir}"/nextflow_report//${current_wf}_timeline.html -with-dag "${current_partage_basedir}"/nextflow_report/${current_wf}_dag.html -with-trace  "${current_partage_basedir}"/nextflow_report/${current_wf}_trace.txt
+
+#TODO DESACTIVER
+#exit 0
 
 cd /data/soft/snvphylnfc/
 
 export NXF_HOME=/data/soft/snvphylnfc/.nextflow
 export NXF_SINGULARITY_CACHEDIR=/data/soft/snvphylnfc/Singularity_Containers
 
-cmd="nextflow run phac-nml/snvphylnfc -c /data/soft/snvphylnfc/myconf/slurm.config -profile singularity --window_size 20 --density_threshold 2  --min_coverage_depth 15 --min_mean_mapping_quality 30 --snv_abundance_ratio 0.75   --input ${snv_phyl_samplesheet} --refgenome ${fasta_reference} --outdir ${snv_phyl_res_dir}"
+current_wf="snvphyl"
+cmd="nextflow run phac-nml/snvphylnfc -c /data/soft/snvphylnfc/myconf/myconfig.config -profile singularity --window_size 20 --density_threshold 2  --min_coverage_depth 15 --min_mean_mapping_quality 30 --snv_abundance_ratio 0.75   --input ${snv_phyl_samplesheet} --refgenome ${fasta_reference} --outdir ${snv_phyl_res_dir} -with-report "${current_partage_basedir}"/nextflow_report/${current_wf}_report.html -with-timeline "${current_partage_basedir}"/nextflow_report//${current_wf}_timeline.html -with-dag "${current_partage_basedir}"/nextflow_report/${current_wf}_dag.html -with-trace  "${current_partage_basedir}"/nextflow_report/${current_wf}_trace.txt"
 
 eval ${cmd}
 
 cd "${phylo_snv_path}"
 export NXF_HOME=/data/devel/phylo_snv_v2/.nextflow
 
-nextflow run main.nf -process.echo -profile slurm --myworflow parse_snvphy_output
+current_wf="parse_snvphyl_output"
+nextflow run main.nf -process.echo -profile slurm --myworflow ${current_wf} -with-report "${current_partage_basedir}"/nextflow_report/${current_wf}_report.html -with-timeline "${current_partage_basedir}"/nextflow_report//${current_wf}_timeline.html -with-dag "${current_partage_basedir}"/nextflow_report/${current_wf}_dag.html -with-trace  "${current_partage_basedir}"/nextflow_report/${current_wf}_trace.txt
+
+
 
 
 
