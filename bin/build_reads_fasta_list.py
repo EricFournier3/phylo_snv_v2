48812833d09a6c10e20d02569175446554007d26
Éric Fournier <foueri01@inspq.qc.ca@slurm10p.inspq.qc.ca>
2024-08-27 12:04:55 -0400

    ef

--- bin/build_reads_fasta_list.py
+++ bin/build_reads_fasta_list.py
@@ -13,9 +13,6 @@ Eric Fournier 2024-08-26
 BIOIN-999
 """
 
-#TODO LOG POUR SAMPLE FILTRE
-
-
 def check_fastp_qc(fastp_json,min_reads_count):
     json_file = open(fastp_json)
     json_load = json.load(json_file)
@@ -27,11 +24,6 @@ def check_fastp_qc(fastp_json,min_reads_count):
 
     return True
 
-#sample name is L00166407001LEGIO_S25
-#L00166407001LEGIO_S25.fastp.json
-#L00166407001LEGIO_S25_R1_001_trimmed_1.fastq.gz
-#L00166407001LEGIO_S25_R2_001_trimmed_2.fastq.gz
-
 parser = argparse.ArgumentParser(description="Filter fastp reads based on count")
 parser.add_argument('--unfiltered-list',help="Ksnp3 unfiltered list",required=True)
 parser.add_argument('--fastp-dir',help="fastp directory",required=True)
@@ -42,42 +34,29 @@ args = parser.parse_args()
 
 unfiltered_list_file = args.unfiltered_list
 filtered_list_file = re.sub(".txt$","_filtered.txt",os.path.basename(unfiltered_list_file))
-print(filtered_list_file)
 
 rejected_samples_file = "rejected_samples.txt"
-rejected_samples_file_handle = open(rejected_samples_file,'w')
+rejected_samples_df = pd.DataFrame(columns=["SAMPLES"])
 
 fastp_dir = args.fastp_dir
 min_reads_count = args.min_reads_count
 filtered_samples_dir = args.filtered_samples_dir 
 
-print("UNFILTERED ",unfiltered_list_file)
-print("FASTP_DIR ", fastp_dir)
-print("min_reads_count ", min_reads_count)
-
-with open(unfiltered_list_file) as rf:
-    for line in rf:
-        print("LINE ",line) 
-
 unfiltered_list_df = pd.read_csv(unfiltered_list_file,sep="\t",index_col=False,header=None) 
-#print(unfiltered_list_df)
 
 with open(filtered_list_file,'w') as wf:
 
     for index, row in unfiltered_list_df.loc[:,].iterrows():
         fasta_path = row[0]
-        print(fasta_path)
         sample = row[1]
         fastp_json = glob.glob(fastp_dir + "/" + sample + "_*.fastp.json")[0]
-        print("JSON ",fastp_json)
 
         qc_pass = check_fastp_qc(fastp_json,min_reads_count)
         
         if qc_pass:
             wf.write(fasta_path + "\t" + sample + "\n")
         else:
-            print(sample, " FAILED")
+            rejected_samples_df = rejected_samples_df.append({'SAMPLES':sample},ignore_index=True)
     
-
-rejected_samples_file_handle.close()
+rejected_samples_df.to_csv(rejected_samples_file,sep="\t",index=False)
 
