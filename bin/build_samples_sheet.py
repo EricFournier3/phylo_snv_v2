450826675e8900e3a2e089eb5be2d70523b8644a
Éric Fournier <foueri01@inspq.qc.ca@slurm10p.inspq.qc.ca>
2024-09-27 10:37:48 -0400

    ef BIOIN-1054

--- bin/build_samples_sheet.py
+++ bin/build_samples_sheet.py
@@ -13,11 +13,41 @@ args = parser.parse_args()
 sample_list_file = args.sample_list_file
 sample_index_file = args.sample_index_file
 
-print("sample_list_file ",sample_list_file)
-print("sample_index_file ",sample_index_file)
+#print("sample_list_file ",sample_list_file)
+#print("sample_index_file ",sample_index_file)
 
 samples_list_df = pd.read_csv(sample_list_file,sep="\t",index_col=False)
 sample_index_df = pd.read_csv(sample_index_file,sep="\t",index_col=False)
 
 merge_df = pd.merge(samples_list_df,sample_index_df,how='inner',on='SAMPLE')
-merge_df.to_csv("/home/foueri01@inspq.qc.ca/TEMP/20240926/merge_df.tsv",sep="\t",index=False)
+
+#TODO DESACTIVER
+'''
+d1 = {'SAMPLE':'L00301982001LEGIO','RUN_NAME':'231205_VL00131_163_AAF7K5GM5_OLD'}
+d2 = {'SAMPLE':'L00301982001LEGIO','RUN_NAME':'231205_VL00131_163_AAF7K5GM5_1'}
+d3 = {'SAMPLE':'L00301982001LEGIO','RUN_NAME':'231205_VL00131_163_AAF7K5GM5_2'}
+d4 = {'SAMPLE':'L00301982001LEGIO','RUN_NAME':'231205_VL00131_163_AAF7K5GM5_3'}
+merge_df = merge_df.append(d1,ignore_index=True)
+merge_df = merge_df.append(d2,ignore_index=True)
+merge_df = merge_df.append(d3,ignore_index=True)
+merge_df = merge_df.append(d4,ignore_index=True)
+'''
+
+merge_df = merge_df.loc[~merge_df['RUN_NAME'].str.contains('_OLD'),:]
+merge_df = merge_df.sort_values(by=['SAMPLE','RUN_NAME'],ascending=[True,True])
+merge_df = merge_df.drop_duplicates(subset=['SAMPLE'],keep='last')
+
+merge_df.to_csv("sample_sheet.tsv",sep="\t",index=False)
+
+
+
+
+
+
+
+
+
+
+
+
+
